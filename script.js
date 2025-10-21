d6386e599a7d58ad769ca7515bbecd6801a3032c
Andrieli Martins <andrieli.martins16@escola.pr.gov.br>
2025-10-21 10:46:37 -0300

    21/10

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
+
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
+
 function mostraPalavrasChave() {
     const texto = document.querySelector("#entrada-de-texto").value;
     const campoResultado = document.querySelector("#resultado-palavrachave");
-    const palavras = texto.split(" ");
-    campoResultado.textContent = palavras.join(", ");
-    
+    const palavras = processaTexto(texto);
+
+    campoResultado.textContent = palavrasChave.join(", ");
+
+}
+
+function processatexto(texto){
+    let palavras = texto.split(/\P{L}+/u);
+
+    return palavras;
 }
