cf19a153c7620f0cccb83d26e8c5f1b0d6b76557
Andrieli Martins <andrieli.martins16@escola.pr.gov.br>
2025-10-21 09:23:28 -0300

    21/10

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-// const botaoMostraPalavras = document.querySelector("#botao-palavrachave")
+ const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
+
+ botaoMostraPalavras.addEventlistener("clik", mostrapalacrasChaves);
+
+ function mostraPalavrasChaves(){
+    alert("fui clicado");
+ }
