12e0e3c5893cd892fe1b5d5f7d8f84c24a0eedae
Andrieli Martins <andrieli.martins16@escola.pr.gov.br>
2025-10-28 10:15:33 -0300

    28/10

--- script.js
+++ script.js
@@ -10,8 +10,20 @@ function mostraPalavrasChave() {
     campoResultado.textContent = palavrasChave.join(", ");
 }
 
-function processatexto(texto){
+function processaTexto(texto){
     let palavras = texto.split(/\P{L}+/u);
+
+    let frequencias = {};
+
+    for(let i of palavras){
+        frequencias[i]=0;
+
+        for (let j of palavras){
+            if (i == j){
+                frequencias[i]++;
+            }
+        }
+    }
 
     return palavras;
 }
